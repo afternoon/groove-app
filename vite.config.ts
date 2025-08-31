055a61a70a81c18071d1aea65f2966cce6713e97
Ben Godfrey <ben@ben2.com>
2025-08-31 23:17:58 +0100

    Fix broken tailwind vite config

--- vite.config.ts
+++ vite.config.ts
@@ -1,16 +1,11 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import tailwindcss from 'tailwindcss'
+import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), tailwindcss()],
   build: {
     minify: false,
-    sourcemap: true,
-  },
-  css: {
-    postcss: {
-      plugins: [tailwindcss()],
-    },
+    sourcemap: true
   }
 })
