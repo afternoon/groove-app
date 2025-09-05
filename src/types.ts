565221dd363601d56ddbfe8048422ae5ea21b390
Ben Godfrey <ben@ben2.com>
2025-09-05 23:29:51 +0100

    Add ProjectContent type and fields to store Track and Section data of the current composition.

--- src/types.ts
+++ src/types.ts
@@ -1,5 +1,33 @@
 import { type Timestamp } from "firebase/firestore";
 
+export const Instrument = {
+  clip: "clip"
+} as const;
+
+export type Instrument = typeof Instrument[keyof typeof Instrument];
+
+export interface Track {
+  id: string;
+  name: string;
+  instrument: Instrument;
+  volume: number; // 0-1
+  isMuted: boolean;
+  isSolo: boolean;
+  sampleUrl: string;
+}
+
+export interface Section {
+  id: string;
+  name: string;
+  enabledTrackIds: string[]; // Track IDs that are enabled in this section
+}
+
+export interface ProjectContent {
+  tempo?: number;
+  tracks?: Track[];
+  sections?: Section[];
+}
+
 export interface Project {
   id: string;
   name?: string;
@@ -7,8 +35,6 @@ export interface Project {
   createdAt: Timestamp;
   isPublic: boolean;
   latestSnapshot?: {
-    content?: {
-      tempo?: number;
-    };
+    content?: ProjectContent;
   };
 }
